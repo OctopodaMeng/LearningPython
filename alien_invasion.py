f933f593d0bdb583c57603db87fc99d8fa830ed2
OctopodaM <zym_95811@163.com>
2021-12-23 16:47:01 +0800

    学习类的使用，添加飞船？

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,12 +1,14 @@
 import sys
 import pygame
+from settings import Settings
 
 class AlienInvasion:
     #管理整个游戏的类
     def __init__(self):
         #初始化整个游戏
         pygame.init()
-        self.screen = pygame.display.set_mode((1200,800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
     def run_game(self):
         #开始循环
@@ -14,6 +16,7 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            self.screen.fill(self.settings.bg_color)
             pygame.display.flip()
 if __name__=='__main__':
     ai = AlienInvasion()
